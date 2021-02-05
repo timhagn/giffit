0721ba689f82f2aaf840b6f4416ced8c741b4941
Tim Hagn <mail@timhagn.com>
2021-02-05 20:39:07 +0100

    add jest-circus

--- jest.config.js
+++ jest.config.js
@@ -123,7 +123,7 @@ module.exports = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: `jest-environment-jsdom`,
+  // testEnvironment: `jest-environment-jsdom`,
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
@@ -148,6 +148,8 @@ module.exports = {
 
   // This option allows use of a custom test runner
   // testRunner: "jasmine2",
+  testEnvironment: 'node',
+  testRunner: 'jest-circus/runner',
 
   // This option sets the URL for the jsdom environment. It is reflected in properties such as location.href
   testURL: 'http://localhost',
